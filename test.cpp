bdcf602213c333b3fb81181bdde5e1231f5f94d0
Michael Bartling <michael.bartling15@gmail.com>
2015-04-04 19:06:25 -0500

    mini test

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "inc/hw_types.h"
+#include "serial.h"
+#include "adc.h"
+#include "os.h"
+
+void Thread1(void){
+  int i = 0;
+  while(1){
+    printf("Hello World! %d", i++);
+    i++;
+  }
+  OS_Kill();
+}
+
+int main(void){
+  OS_Init();           // initialize, disable interrupts
+
+  OS_AddThread(&Thread1,128,0); 
+  OS_Launch(TIME_1MS); // doesn't return, interrupts enabled in here
+  return 0;            // this never executes
+}
