3391017c6987a36e4e5ac32b2943d8dcab337fc5
Michael Bartling <michael.bartling15@gmail.com>
2015-04-21 20:43:10 -0500

    Added median filter

---
+++ algorithms/median.h
@@ -0,0 +1,8 @@
+#ifdef __MEDIAN_H__
+#define __MEDIAN_H__
+#include <stdlib.h>
+#include <stdint.h>
+
+// uint32_t median(uint32_t* buffer, int numInts);
+uint32_t median_filt(uint32_t* buffer, int numInts)
+#endif /*__MEDIAN_H__*/
